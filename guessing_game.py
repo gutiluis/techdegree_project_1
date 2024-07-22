c960e3b6987363c577d0a010e622730c0315419a
gutiluis <gutierrez.luis@icloud.com>
2024-07-22 13:07:14 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -5,6 +5,12 @@ Project 1 - The Number Guessing Game
 """
 import random, time, sys
 
+
+
+def main():
+	start_game()
+	continue_game()
+
 def start_game():
 	print("############################################################")
 	print("          ######################### Welcome Player! ##############################")
@@ -12,23 +18,34 @@ def start_game():
 	print("          ########################################################################")
 	time.sleep(1)
 	random_number = random.randint(1,10)
-	guesses = 1
-	secret_number = int(input("Please enter a number between 1 and 10:    "))
+	guesses = 0
+	secret_number = True
 	while secret_number != random_number:
-		if secret_number < random_number:
-			secret_number = int(input("It's higher:  "))
-			guesses += 1
-		else: 
-			secret_number > int(input("It's lower:  "))
-			guesses += 1
+		try:
+			secret_number = int(input("Please enter a number between 1 and 10:    "))
+			if secret_number < random_number:
+				print("It's higher")
+				guesses += 1
+			if secret_number > random_number:
+				print("It's lower")
+				guesses += 1
+		except ValueError:
+			print("not valid")
+	secret_number = random_number
+	guesses += 1
 	print("Congratulations it took you",str(guesses),"attempts. The secret number is",(secret_number),)
+start_game()
+
+def continue_game():
 	continue_game = input("Play again?  ")
 	while continue_game.lower() == "y":
 		start_game()
 	else:
+		continue_game.lower() == ("no", "n")
 		print("Game Over")
 		sys.exit()
+continue_game()
+#		sys.exit()
 
-
-start_game()
+main()
 
